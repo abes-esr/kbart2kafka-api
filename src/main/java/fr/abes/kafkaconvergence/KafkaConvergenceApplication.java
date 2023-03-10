647bb849361e3ac4d65c3622a09b02ba5ecc77a4
villiseck <jvk@abes.fr>
2023-03-10 16:39:14 +0100

    Initialisation projet

---
+++ src/main/java/fr/abes/kafkaconvergence/KafkaConvergenceApplication.java
@@ -0,0 +1,12 @@
+package fr.abes.kafkaconvergence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaConvergenceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaConvergenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/abes/kafkaconvergence/KafkaConvergenceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.abes.kafkaconvergence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaConvergenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
