779f7822848a6adb9a190c687e3fb8dc5599a3b6
EryneKL <97091460+EryneKL@users.noreply.github.com>
2023-03-31 11:12:28 +0200

    FEAT : CDE-88      - test de la class BestPpnServiceTest.java

--- src/test/java/fr/abes/kafkaconvergence/service/BestPpnServiceTest.java
+++ src/test/java/fr/abes/kafkaconvergence/service/BestPpnServiceTest.java
@@ -1,15 +1,25 @@
 package fr.abes.kafkaconvergence.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import fr.abes.kafkaconvergence.dto.LigneKbartDto;
 import fr.abes.kafkaconvergence.dto.LoggerResultDto;
+import fr.abes.kafkaconvergence.dto.PpnWithTypeDto;
+import fr.abes.kafkaconvergence.dto.ResultWsSudocDto;
+import fr.abes.kafkaconvergence.exception.IllegalPpnException;
 import fr.abes.kafkaconvergence.logger.Logger;
+import fr.abes.kafkaconvergence.utils.TYPE_SUPPORT;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.TestPropertySource;
 
+import java.io.IOException;
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -20,13 +30,13 @@ import java.util.Map;
 class BestPpnServiceTest {
 
     @Autowired
-    BestPpnService service;
+    BestPpnService bestPpnService;
 
     @MockBean
     NoticeService noticeService;
 
     @MockBean
-    WsService wsService;
+    WsService service;
 
     @MockBean
     Logger logger;
@@ -47,29 +57,56 @@ class BestPpnServiceTest {
     long scoreDat2Ppn;
 
     @Test
-    void getList(){
+    void sortByBestPpn(){
         Map<String, Long> list = new HashMap<>();
         list.put("444444444", Long.valueOf(4));
         list.put("777777777", Long.valueOf(7));
         list.put("222222222", Long.valueOf(2));
         list.put("666666666", Long.valueOf(6));
-        List<String> result = service.sortByBestPpn(list);
+        List<String> result = bestPpnService.sortByBestPpn(list);
         Assertions.assertEquals(result.get(0), "777777777");
     }
 
     @Test
-    void getBestPpn() {
-    }
+    @DisplayName("feedPpnListFromOnline")
+    void getBestPpn() throws IllegalPpnException, IOException, NoSuchFieldException {
+        //  Create a PpnWithTypeDto
+        PpnWithTypeDto ppnWithType = new PpnWithTypeDto();
+        ppnWithType.setPpn("111111111");
+        ppnWithType.setType(TYPE_SUPPORT.ELECTRONIQUE);
+        //  Create a List of PpnWithListDto
+        List<PpnWithTypeDto> ppnWithTypeDto = new ArrayList<>();
+        ppnWithTypeDto.add(ppnWithType);
+        //  Create a ResultWsSudocDto
+        ResultWsSudocDto result = new ResultWsSudocDto();
+        result.setPpns(ppnWithTypeDto);
 
-    @Test
-    void feedPpnListFromOnline() {
-    }
+        //  Create a LigneKbartDto
+        LigneKbartDto kbart = new LigneKbartDto();
+        kbart.setOnline_identifier("1292-8399");
+        kbart.setPublication_type("serial");
+        kbart.setDate_monograph_published_online("DateOnline");
+        kbart.setPublication_title("Titre");
+        kbart.setFirst_author("Auteur");
+        kbart.setDate_monograph_published_print("DatePrint");
 
-    @Test
-    void feedPpnListFromPrint() {
-    }
+        //  Mock the WsService
+        Mockito.when(service.callOnlineId2Ppn("serial", "1292-8399", "UrlProvider")).thenReturn(result);
+//        Mockito.when(service.callPrintId2Ppn("serial", "1292-8399", "UrlProvider")).thenReturn(result);
+//        Mockito.when(service.callDat2Ppn("serial", "1292-8399", "")).thenReturn(result);
 
-    @Test
-    void feedPpnListFromDat() {
+        Field field = bestPpnService.getClass().getDeclaredField("ppnElecList");
+        field.setAccessible(true);
+
+        List<String> test = bestPpnService.getBestPpn(kbart, "UrlProvider");
+
+        //  Test avec 1 ppn type électronique
+        Assertions.assertEquals(test.get(0), result.getPpns().get(0).getPpn());
+
+        //  Test avec 3 ppns type électronique
+
+        // Test avec 1 ppn type imprimé
+
+
     }
 }
