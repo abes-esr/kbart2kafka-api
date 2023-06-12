9be0e71664f2090bfaed325ab361dcf2e8680c97
SamuelQuetin <samquetinuel@gmail.com>
2023-06-12 16:24:03 +0200

    fix check URL

--- src/main/java/fr/abes/kafkaconvergence/service/CheckUrlService.java
+++ src/main/java/fr/abes/kafkaconvergence/service/CheckUrlService.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -31,25 +30,29 @@ public class CheckUrlService {
         String domain = Utils.extractDomainFromUrl(titleUrl);
         //récupération notice dans la base pour analyse
         NoticeXml notice = noticeService.getNoticeByPpn(ppn);
-        List<Datafield> zones856 = notice.getZoneDollarUWithoutDollar5("856");
-        for(Datafield zone : zones856) {
-            for (SubField sousZone : zone.getSubFields().stream().filter(sousZone -> sousZone.getCode().equals("u")).toList()) {
-                if (sousZone.getValue().contains(domain)) {
-                    log.debug("Url trouvée dans 856");
-                    return true;
+        if(notice.getTypeDocument().charAt(1) == 'a') {
+            List<Datafield> zones856 = notice.getZoneDollarUWithoutDollar5("856");
+            for(Datafield zone : zones856) {
+                for (SubField sousZone : zone.getSubFields().stream().filter(sousZone -> sousZone.getCode().equals("u")).toList()) {
+                    if (sousZone.getValue().contains(domain)) {
+                        log.debug("Url trouvée dans 856");
+                        return true;
+                    }
                 }
             }
-        }
-        List<Datafield> zone859 = notice.getZoneDollarUWithoutDollar5("859");
-        for (Datafield zone : zone859) {
-            for (SubField sousZone : zone.getSubFields().stream().filter(sousZone -> sousZone.getCode().equals("u")).toList()) {
-                if (sousZone.getValue().contains(domain)) {
-                    log.debug("Url trouvée dans 859");
-                    return true;
+            List<Datafield> zone859 = notice.getZoneDollarUWithoutDollar5("859");
+            for (Datafield zone : zone859) {
+                for (SubField sousZone : zone.getSubFields().stream().filter(sousZone -> sousZone.getCode().equals("u")).toList()) {
+                    if (sousZone.getValue().contains(domain)) {
+                        log.debug("Url trouvée dans 859");
+                        return true;
+                    }
                 }
             }
+            log.error("Pas de correspondance trouvée dans la notice avec l'url du provider.");
+            return false;
+        } else {
+            return true;
         }
-        log.error("Pas de correspondance trouvée dans la notice avec l'url du provider.");
-        return false;
     }
 }
