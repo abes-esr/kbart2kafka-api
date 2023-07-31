d64ee42fab7626240eecc11d0c79c79576736d39
EryneKL <97091460+EryneKL@users.noreply.github.com>
2023-07-31 09:15:30 +0200

    Feat : CDE-214 Déplacer la création du provider / package      - suppression d'un contrôle inutile (CheckFile.java)

--- src/main/java/fr/abes/kbart2kafka/utils/CheckFiles.java
+++ src/main/java/fr/abes/kbart2kafka/utils/CheckFiles.java
@@ -14,7 +14,6 @@ public class CheckFiles {
 
     public static void detectFileName(File file) throws IllegalFileFormatException {
         String filename = file.getName();
-        filename = filename.replace("\\", "/");
         if(!filename.matches("([a-zA-Z0-9\\-_]+_[a-zA-Z0-9\\-]+_)+(\\d{4}-\\d{2}-\\d{2})+(_FORCE)?+(.tsv)$")){
             log.error("Message envoyé : {}", "Le nom du fichier n'est pas correct");
             throw new IllegalFileFormatException("Le nom du fichier "+ filename +" n'est pas correct");
