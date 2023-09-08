1a91c42c48cbc7bf376a40daa53677f7962028c6
SamuelQuetin <samquetinuel@gmail.com>
2023-09-08 08:58:31 +0200

    modif config docker

--- src/main/java/fr/abes/kbart2kafka/configuration/SenderConfig.java
+++ src/main/java/fr/abes/kbart2kafka/configuration/SenderConfig.java
@@ -5,14 +5,17 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.transaction.KafkaTransactionManager;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
+@EnableKafka
 public class SenderConfig {
 
     @Value("${spring.kafka.producer.bootstrap-servers}")
@@ -33,11 +36,20 @@ public class SenderConfig {
 
     @Bean
     public ProducerFactory<String, String> producerFactory() {
+        DefaultKafkaProducerFactory<String, String> factory = new DefaultKafkaProducerFactory<>(producerConfigs());
+        factory.setTransactionIdPrefix(transactionIdPrefix);
         return new DefaultKafkaProducerFactory<>(producerConfigs());
+    }
+
+    @Bean
+    public KafkaTransactionManager<String, String> kafkaTransactionManager(){
+        return new KafkaTransactionManager<>(producerFactory());
     }
 
     @Bean
     public KafkaTemplate<String, String> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
     }
+
+
 }
