1331b6f5fd44b17f3d045e6aaa680bda3b75146c
pierre-maraval <pierre.maraval@gmail.com>
2023-11-21 09:00:15 +0100

    FEAT : Ajout multi threading

--- src/main/java/fr/abes/kbart2kafka/Kbart2kafkaApplication.java
+++ src/main/java/fr/abes/kbart2kafka/Kbart2kafkaApplication.java
@@ -1,5 +1,7 @@
 package fr.abes.kbart2kafka;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import fr.abes.kbart2kafka.dto.Header;
 import fr.abes.kbart2kafka.dto.LigneKbartDto;
 import fr.abes.kbart2kafka.exception.IllegalFileFormatException;
@@ -7,132 +9,157 @@ import fr.abes.kbart2kafka.exception.IllegalProviderException;
 import fr.abes.kbart2kafka.kafka.TopicProducer;
 import fr.abes.kbart2kafka.utils.CheckFiles;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Slf4j
 @EnableKafka
 @SpringBootApplication
 public class Kbart2kafkaApplication implements CommandLineRunner {
 
-	@Value("${kbart.header}")
-	private String kbartHeader;
+    @Value("${kbart.header}")
+    private String kbartHeader;
 
-	@Autowired
-	private TopicProducer topicProducer;
+    private final TopicProducer topicProducer;
 
-	public static void main(String[] args) {
-		SpringApplication.run(Kbart2kafkaApplication.class, args);
-	}
+    private final ObjectMapper mapper;
 
-	/**
-	 * Méthode run qui lit un fichier tsv et en extrait les données ligne par ligne pour les envoyer dans un topic kafka
-	 * @param args fichier tsv placé dans un répertoire défini
-	 * @throws IOException Exception levée lorsque aucun fichier tsv n'a été trouvé.
-	 */
-	@Override
-	@Transactional // on spécifie la class qui fait rollback, par defaut c'est toutes les classes qui ne sont pas gérées càd : tout sauf IOException
-	public void run(String... args) throws IOException {
+    ExecutorService executor = Executors.newFixedThreadPool(5);
 
-		//	Contrôle de la présence d'un paramètre au lancement de Kbart2kafkaApplication
-		if(args.length == 0 || args[0] == null || args[0].trim().isEmpty()) {
-			log.error("Message envoyé : {}", "Le chemin d'accès au fichier tsv n'a pas été trouvé dans les paramètres de l'application");
-		} else {
-			log.info("Debut envois kafka de : " + args[0]);
-			//	Récupération du chemin d'accès au fichier
-			File tsvFile = new File(args[0]);
+    public Kbart2kafkaApplication(TopicProducer topicProducer, ObjectMapper mapper) {
+        this.topicProducer = topicProducer;
+        this.mapper = mapper;
+    }
 
-			try {
-				//	Appelle du service de vérification de fichier
-				CheckFiles.verifyFile(tsvFile, kbartHeader);
+    public static void main(String[] args) {
+        SpringApplication.run(Kbart2kafkaApplication.class, args);
+    }
 
-				// Calcul du nombre total de ligne
-				Scanner kbartTotalLines = new Scanner(tsvFile);
-				int totalNumberOfLine = 0;
-				while(kbartTotalLines.hasNextLine()){
-					String ligneKbart = kbartTotalLines.nextLine();
-					if(!ligneKbart.contains(kbartHeader)) {
-						totalNumberOfLine ++;
-					}
-				}
+    /**
+     * Méthode run qui lit un fichier tsv et en extrait les données ligne par ligne pour les envoyer dans un topic kafka
+     *
+     * @param args fichier tsv placé dans un répertoire défini
+     * @throws IOException Exception levée lorsque aucun fichier tsv n'a été trouvé.
+     */
+    @Override
+    @Transactional
+    // on spécifie la class qui fait rollback, par defaut c'est toutes les classes qui ne sont pas gérées càd : tout sauf IOException
+    public void run(String... args) throws IOException {
 
-				// Compteur du nombre de lignes dans le kbart
-				Scanner kbart = new Scanner(tsvFile);
-				int lineCounter = 0;
+        //	Contrôle de la présence d'un paramètre au lancement de Kbart2kafkaApplication
+        if (args.length == 0 || args[0] == null || args[0].trim().isEmpty()) {
+            log.error("Message envoyé : {}", "Le chemin d'accès au fichier tsv n'a pas été trouvé dans les paramètres de l'application");
+        } else {
+            log.info("Debut envois kafka de : " + args[0]);
+            //	Récupération du chemin d'accès au fichier
+            File tsvFile = new File(args[0]);
 
-				// Création du header et ajout du nombre total de lignes
-				Header kafkaHeader = new Header(tsvFile.getName(), totalNumberOfLine);
+            try {
+                //	Appelle du service de vérification de fichier
+                CheckFiles.verifyFile(tsvFile, kbartHeader);
 
-				while (kbart.hasNextLine()) {
-					String ligneKbart = kbart.nextLine();
-					if (!ligneKbart.contains(kbartHeader)) {
-						lineCounter ++;
+                // Calcul du nombre total de ligne
+                Scanner kbartTotalLines = new Scanner(tsvFile);
+                int totalNumberOfLine = 0;
+                while (kbartTotalLines.hasNextLine()) {
+                    String ligneKbart = kbartTotalLines.nextLine();
+                    if (!ligneKbart.contains(kbartHeader)) {
+                        totalNumberOfLine++;
+                    }
+                }
 
-						// Crée un nouvel objet dto, set les différentes parties et envoi au service topicProducer
-						String[] tsvElementsOnOneLine = ligneKbart.split("\t");
-						LigneKbartDto ligneKbartDto = constructDto(tsvElementsOnOneLine);
+                // Compteur du nombre de lignes dans le kbart
+                Scanner kbart = new Scanner(tsvFile);
+                int lineCounter = 0;
 
-						//	Envoi de la ligne kbart dans le producer
-						kafkaHeader.setCurrentLine(lineCounter);
-							topicProducer.sendLigneKbart(ligneKbartDto, kafkaHeader);
-					}
-				}
-				// Envoi du message de fin de traitement dans le producer "OK"
-				topicProducer.sendOk(kafkaHeader);
-			} catch (IOException e) {
-				throw new IOException(e);
-			} catch (IllegalFileFormatException | IllegalProviderException e) {
-				throw new RuntimeException(e);
-			}
-		}
-	}
+                // Création du header et ajout du nombre total de lignes
+                Header kafkaHeader = new Header(tsvFile.getName(), totalNumberOfLine);
 
-	/**
-	 * Construction de la dto
-	 *
-	 * @param line ligne en entrée
-	 * @return Un objet DTO initialisé avec les informations de la ligne
-	 */
-	private LigneKbartDto constructDto(String[] line) {
-		LigneKbartDto kbartLineInDtoObject = new LigneKbartDto();
-		kbartLineInDtoObject.setPublication_title(line[0]);
-		kbartLineInDtoObject.setPrint_identifier(line[1]);
-		kbartLineInDtoObject.setOnline_identifier(line[2]);
-		kbartLineInDtoObject.setDate_first_issue_online(line[3]);
-		kbartLineInDtoObject.setNum_first_vol_online(Integer.getInteger(line[4]));
-		kbartLineInDtoObject.setNum_first_issue_online(Integer.getInteger(line[5]));
-		kbartLineInDtoObject.setDate_last_issue_online(line[6]);
-		kbartLineInDtoObject.setNum_last_vol_online(Integer.getInteger(line[7]));
-		kbartLineInDtoObject.setNum_last_issue_online(Integer.getInteger(line[8]));
-		kbartLineInDtoObject.setTitle_url(line[9]);
-		kbartLineInDtoObject.setFirst_author(line[10]);
-		kbartLineInDtoObject.setTitle_id(line[11]);
-		kbartLineInDtoObject.setEmbargo_info(line[12]);
-		kbartLineInDtoObject.setCoverage_depth(line[13]);
-		kbartLineInDtoObject.setNotes(line[14]);
-		kbartLineInDtoObject.setPublisher_name(line[15]);
-		kbartLineInDtoObject.setPublication_type(line[16]);
-		kbartLineInDtoObject.setDate_monograph_published_print(line[17]);
-		kbartLineInDtoObject.setDate_monograph_published_online(line[18]);
-		kbartLineInDtoObject.setMonograph_volume(Integer.getInteger(line[19]));
-		kbartLineInDtoObject.setMonograph_edition(line[20]);
-		kbartLineInDtoObject.setFirst_editor(line[21]);
-		kbartLineInDtoObject.setParent_publication_title_id(line[22]);
-		kbartLineInDtoObject.setPreceding_publication_title_id(line[23]);
-		kbartLineInDtoObject.setAccess_type(line[24]);
-		// Vérification de la présence d'un best ppn déjà renseigné dans le kbart
-		if (line.length == 26){
-			kbartLineInDtoObject.setBestPpn(line[25]);
-		}
-		return kbartLineInDtoObject;
-	}
+                while (kbart.hasNextLine()) {
+                    String ligneKbart = kbart.nextLine();
+                    if (!ligneKbart.contains(kbartHeader)) {
+                        lineCounter++;
+
+                        // Crée un nouvel objet dto, set les différentes parties et envoi au service topicProducer
+                        String[] tsvElementsOnOneLine = ligneKbart.split("\t");
+                        LigneKbartDto ligneKbartDto = constructDto(tsvElementsOnOneLine);
+
+                        //	Envoi de la ligne kbart dans le producer
+                        kafkaHeader.setCurrentLine(lineCounter);
+
+                        executor.submit(() -> {
+                            try {
+                                CompletableFuture<SendResult<String, String>> result = topicProducer.sendLigneKbart(ligneKbartDto, kafkaHeader);
+                                log.debug("Message envoyé : {}", mapper.writeValueAsString(result.get().getProducerRecord().value()));
+                            } catch (JsonProcessingException | InterruptedException | ExecutionException e) {
+                                throw new RuntimeException(e);
+                            }
+                        });
+                    }
+                }
+                // Envoi du message de fin de traitement dans le producer "OK"
+                executor.submit(() -> topicProducer.sendOk(kafkaHeader));
+            } catch (IOException e) {
+                throw new IOException(e);
+            } catch (IllegalFileFormatException | IllegalProviderException e) {
+                throw new RuntimeException(e);
+            }
+            finally {
+                executor.shutdown();
+            }
+        }
+    }
+
+    /**
+     * Construction de la dto
+     *
+     * @param line ligne en entrée
+     * @return Un objet DTO initialisé avec les informations de la ligne
+     */
+    private LigneKbartDto constructDto(String[] line) {
+        LigneKbartDto kbartLineInDtoObject = new LigneKbartDto();
+        kbartLineInDtoObject.setPublication_title(line[0]);
+        kbartLineInDtoObject.setPrint_identifier(line[1]);
+        kbartLineInDtoObject.setOnline_identifier(line[2]);
+        kbartLineInDtoObject.setDate_first_issue_online(line[3]);
+        kbartLineInDtoObject.setNum_first_vol_online(Integer.getInteger(line[4]));
+        kbartLineInDtoObject.setNum_first_issue_online(Integer.getInteger(line[5]));
+        kbartLineInDtoObject.setDate_last_issue_online(line[6]);
+        kbartLineInDtoObject.setNum_last_vol_online(Integer.getInteger(line[7]));
+        kbartLineInDtoObject.setNum_last_issue_online(Integer.getInteger(line[8]));
+        kbartLineInDtoObject.setTitle_url(line[9]);
+        kbartLineInDtoObject.setFirst_author(line[10]);
+        kbartLineInDtoObject.setTitle_id(line[11]);
+        kbartLineInDtoObject.setEmbargo_info(line[12]);
+        kbartLineInDtoObject.setCoverage_depth(line[13]);
+        kbartLineInDtoObject.setNotes(line[14]);
+        kbartLineInDtoObject.setPublisher_name(line[15]);
+        kbartLineInDtoObject.setPublication_type(line[16]);
+        kbartLineInDtoObject.setDate_monograph_published_print(line[17]);
+        kbartLineInDtoObject.setDate_monograph_published_online(line[18]);
+        kbartLineInDtoObject.setMonograph_volume(Integer.getInteger(line[19]));
+        kbartLineInDtoObject.setMonograph_edition(line[20]);
+        kbartLineInDtoObject.setFirst_editor(line[21]);
+        kbartLineInDtoObject.setParent_publication_title_id(line[22]);
+        kbartLineInDtoObject.setPreceding_publication_title_id(line[23]);
+        kbartLineInDtoObject.setAccess_type(line[24]);
+        // Vérification de la présence d'un best ppn déjà renseigné dans le kbart
+        if (line.length == 26) {
+            kbartLineInDtoObject.setBestPpn(line[25]);
+        }
+        return kbartLineInDtoObject;
+    }
 }
--- src/main/java/fr/abes/kbart2kafka/kafka/TopicProducer.java
+++ src/main/java/fr/abes/kbart2kafka/kafka/TopicProducer.java
@@ -6,13 +6,26 @@ import fr.abes.kbart2kafka.dto.Header;
 import fr.abes.kbart2kafka.dto.LigneKbartDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.header.internals.RecordHeader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.CompletableFuture;
 
 /**
  * Producer Kafka qui envoi les lignes d'un kbart et les messages de fin de traitement dans un topic kafka
@@ -28,32 +41,46 @@ public class TopicProducer {
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
+    @Autowired
+    private PlatformTransactionManager kafkaTransactionManager;
+
     private final ObjectMapper mapper;
 
     /**
      * Envoi une ligne d'un kbart dans un topic kafka
-     * @param kbart un fichier kbart
+     *
+     * @param kbart  un fichier kbart
      * @param header le nom du fichier avec son extension, la date, le nombre de ligne du kbart
+     * @return
      * @throws JsonProcessingException Exception pour tous les problèmes de traitement de contenu JSON
      */
-    public void sendLigneKbart(LigneKbartDto kbart, Header header) throws JsonProcessingException {
-        Message<String> message = MessageBuilder
-                .withPayload(mapper.writeValueAsString(kbart))
-                .setHeader(KafkaHeaders.TOPIC, topicKbart)
-                .setHeader("FileName", header.getFileName())
-                .setHeader("CurrentLine", header.getCurrentLine())
-                .setHeader("TotalLine", header.getTotalNumberOfLine())
-                .build();
-        log.info("envoi : " + message);
-        kafkaTemplate.send(message);
-        log.debug("Message envoyé : {}", mapper.writeValueAsString(kbart));
+    public CompletableFuture<SendResult<String, String>> sendLigneKbart(LigneKbartDto kbart, Header header) throws JsonProcessingException {
+        DefaultTransactionDefinition def = new DefaultTransactionDefinition();
+        def.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
+        TransactionStatus status = kafkaTransactionManager.getTransaction(def);
+        List<org.apache.kafka.common.header.Header> headers = new ArrayList<>();
+        headers.add(new RecordHeader("FileName", header.getFileName().getBytes(StandardCharsets.UTF_8)));
+        headers.add(new RecordHeader("CurrentLine", String.valueOf(header.getCurrentLine()).getBytes(StandardCharsets.UTF_8)));
+        headers.add(new RecordHeader("TotalLine", String.valueOf(header.getTotalNumberOfLine()).getBytes(StandardCharsets.UTF_8)));
+        ProducerRecord<String, String> record = new ProducerRecord<>(topicKbart, new Random().nextInt(5), "", mapper.writeValueAsString(kbart), headers);
+
+//        Message<String> message = MessageBuilder
+//                .withPayload(mapper.writeValueAsString(kbart))
+//                .setHeader(KafkaHeaders.TOPIC, topicKbart)
+//                .setHeader("FileName", header.getFileName())
+//                .setHeader("CurrentLine", header.getCurrentLine())
+//                .setHeader("TotalLine", header.getTotalNumberOfLine())
+//                .build();
+        return kafkaTemplate.send(record);
     }
 
     /**
      * Envoi le message de fin de traitement d'un kbart dans un topic kafka
+     *
      * @param header le nom du fichier avec son extension, la date, le nombre de ligne du kbart
+     * @return
      */
-    public void sendOk(Header header) {
+    public CompletableFuture<SendResult<String, String>> sendOk(Header header) {
 
         Message<String> message = MessageBuilder
                 .withPayload("OK")
@@ -63,7 +90,6 @@ public class TopicProducer {
                 .setHeader("TotalLine", header.getTotalNumberOfLine())
                 .build();
 
-        kafkaTemplate.send(message);
-        log.debug("Message envoyé : {}", "Le kbart " + header.getFileName() + " a été traité dans son intégralité.");
+        return kafkaTemplate.send(message);
     }
 }
