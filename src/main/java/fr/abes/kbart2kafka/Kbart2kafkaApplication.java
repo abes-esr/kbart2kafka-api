66f1fb4b9b309848edb624d7222c20bb655be9e3
SamuelQuetin <quetin@abes.fr>
2024-10-29 10:36:34 +0100

    ajout log quand fichier existe déjà

--- src/main/java/fr/abes/kbart2kafka/Kbart2kafkaApplication.java
+++ src/main/java/fr/abes/kbart2kafka/Kbart2kafkaApplication.java
@@ -66,8 +66,13 @@ public class Kbart2kafkaApplication implements CommandLineRunner {
                 log.info("Traitement refusé du fichier " + tsvFile.getName());
             } finally {
                 File fichierLog = new File(tsvFile.getPath().replace(".tsv",".log"));
+                if(fichierLog.exists()) {
+                    log.debug("Fichier " + fichierLog.getName() + " existe déjà");
+                }
                 if(fichierLog.createNewFile())
                     log.debug("Création du fichier " + fichierLog.getName());
+                else
+                    log.debug("Erreur lors de la création de " + fichierLog.getName());
             }
         }
         long endTime = System.currentTimeMillis();
